73208d0ead15df1f927de5461bfd34907aa68924
AjManzoor <ajmal_manzoor@hotmail.co.uk>
2023-10-17 11:38:53 +0100

    digivolve text added

--- digivolve.gd
+++ digivolve.gd
@@ -5,12 +5,27 @@ extends Scene
 @export var hide_image_timer : Timer
 @export var digivolve_timer : Timer
 
+@onready var digivolve_label : RichTextLabel = $DigivolveLabel
+@onready var next_button : Button = $NextButton
+
 signal level_changed() 
 
 var is_current_digimon : bool = true
 var is_digivolving : bool
 
+func hide_items():
+	digivolve_label.hide()
+	next_button.hide()
+	
+func show_items():
+	digivolve_label.show()
+	next_button.show()
+	
+func  update_text():
+	digivolve_label.bbcode_text = "Your " + current_digimon + " has digivolved to " + next_digimon
+
 func on_enter():
+	hide_items()
 	is_digivolving = true
 	digivolve_timer.start()
 	print("enter")
@@ -58,12 +73,16 @@ func _on_hide_image_timer_timeout():
 		is_current_digimon = true
 		current_digimon_texture.show()
 
-
 func _on_digivolve_timer_timeout():
 	is_digivolving = false
 	var image = load_digimon_texture(next_digimon)
 	next_digimon_texture.texture = ImageTexture.create_from_image(image)
-	#emit_signal("level_changed", "DigiEvo")
+	show_items()
+	update_text()
 	
 func go_to_next_scene():
-	pass
+	emit_signal("level_changed", "DigiEvo")
+
+func _on_button_pressed():
+	print("clicked")
+	go_to_next_scene()
